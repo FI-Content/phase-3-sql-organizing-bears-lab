0eb0020c7df4fd4c23a0f2758d57b47af0fac920
Franck <gwadafrank@gmail.com>
2022-10-25 02:38:38 +0300

    create database and prform crud operations

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Mr. Chocolate", 3, "M", "black", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Rowdy", 4, "F", "White", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Tabitha", 3, "M", "black", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Sergeant Brown", 4, "F", "White", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Melissa", 3, "M", "black", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Grinch", 4, "F", "White", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES("Wendy", 3, "M", "black", "mild", true);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES(NULL , 4, "F", "White", "mild", true);
